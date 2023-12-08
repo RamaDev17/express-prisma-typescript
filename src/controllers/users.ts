d3c15cb5fb3005c0e6fdbded192cb39cad74f01c
Rizki-SMAFG <rizki.it@smafg.sch.id>
2023-12-08 08:33:15 +0700

    feat: try catch in userController

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,46 +4,75 @@ const prisma = new PrismaClient()
 
 const postUser = async (req: Request, res: Response) => {
     const { name, email, password } = req.body
-    const result = await prisma.users.create({
-        data: {
-            name,
-            email,
-            password
-        }
-    })
+    try {
+        const result = await prisma.users.create({
+            data: {
+                name,
+                email,
+                password
+            }
+        })
 
-    res.json({ message: 'User Creates', data: result })
+        res.status(201).json({ message: 'User Creates', data: result })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error
+        })
+    }
 }
 
 const getAllUser = async (req: Request, res: Response) => {
-    const result = await prisma.users.findMany()
-    res.json({ message: 'Users List', data: result })
+    try {
+        const result = await prisma.users.findMany()
+        res.json({ message: 'Users List', data: result })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error
+        })
+    }
 }
 
 const updateUser = async (req: Request, res: Response) => {
     const { id } = req.params
-    const { name, email, password } = req.body
+    const { name, email, password, is_verified } = req.body
 
-    const result = await prisma.users.update({
-        data: {
-            name,
-            email,
-            password
-        },
-        where: {
-            id: Number(id)
-        }
-    })
-    res.json({ message: `User ${id} updated`, data: result })
+    try {
+        const result = await prisma.users.update({
+            data: {
+                name,
+                email,
+                password,
+                is_verified
+            },
+            where: {
+                id: Number(id)
+            }
+        })
+        res.json({ message: `User ${id} updated`, data: result })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error
+        })
+    }
 }
 
 const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params
-    const result = await prisma.users.delete({
-        where: { id: Number(id) }
-    })
+    try {
+        await prisma.users.delete({
+            where: { id: Number(id) }
+        })
 
-    res.json({ message: `User ${id} deleted` })
+        res.json({ message: `User ${id} deleted` })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error
+        })
+    }
 }
 
 export {
